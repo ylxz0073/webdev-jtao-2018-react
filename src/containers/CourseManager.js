5ab7731df0c09bb32e09153a38a9925b92bc0c32
ylxz0073 <jiapengtao@yahoo.com>
2018-06-01 23:00:57 -0700

    add course card container

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
+import CourseCard from './CourseCard'
 
 export default class CourseManager extends Component {
     render() {
         return (
             <div className="container-fluid">
-            <h1>Course Manager</h1>
+                <h1>Course Manager</h1>
+                <div class="card-deck">
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                    <CourseCard/>
+                </div>
             </div>
         )
     }
