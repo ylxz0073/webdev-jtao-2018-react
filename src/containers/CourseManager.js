fac1bf51bb9af4c9f66d5b0d61493ef7fb705ad5
ylxz0073 <jiapengtao@yahoo.com>
2018-06-01 23:40:23 -0700

    add course editor

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
 import CourseCard from '../Components/CourseCard'
 import ModuleList from './ModuleList'
+import LessonTabs from './LessonTabs'
+import CourseEditor from './CourseEditor'
 
 export default class CourseManager extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <CourseEditor/>
+                <br/>
+                <LessonTabs/>
                 <ModuleList/>
                 <div class="card-deck">
                     <CourseCard/>
